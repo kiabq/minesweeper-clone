5edbae2f18aa308c9bba801266c3c83fd2686652
kiabq <joprograms@gmail.com>
2022-06-10 04:38:56 +0000

    Added initClues function to create clues array on first click

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { type } from 'os';
 import * as React from 'react';
 import styles from './App.module.css'
 
 // Initial state shape
-interface gameArr {
+interface gameState {
   gArr: Array<number[]>,
   mArr: Set<number>,
-  cArr: Array<number>,
+  cArr: Array<number | null>,
   initialized: boolean,
   gameOver: boolean
 }
@@ -33,7 +34,25 @@ function pArr(inputArr: Array<number[]>, value: number) {
   ]
 }
 
-class App extends React.Component<{}, gameArr>{
+function borderCheck(inputArr: Array<number[]>, gameArr: Array<number[]>, index: number, inputObj: Set<number> | Array<any>) {
+  inputArr.forEach((item: any) => {
+    if (item !== undefined) {
+      if (gameArr[index][0] === 0 && item[0] - 9 === -1) {
+        return;
+      } else if (gameArr[index][0] === 8 && item[0] - 1 === -1) {
+        return;
+      } else if (inputObj instanceof Set) {
+        inputObj.add(gameArr.indexOf(item));
+      } else {
+        inputObj.push(gameArr.indexOf(item));
+      }
+    } else if (item === undefined) {
+      return;
+    }
+  })
+}
+
+class App extends React.Component<{}, gameState>{
 
   constructor(props: any) {
     super(props);
@@ -51,16 +70,16 @@ class App extends React.Component<{}, gameArr>{
   componentDidMount() {
     window.addEventListener('contextmenu', checkClick);
 
-    let temp: number[][] = [];
-    let main: number[][] = [[0,1,2,3,4,5,6,7,8], [0,1,2,3,4,5,6,7,8]];
+    let tdaGameboard: number[][] = [];
+    let tdaGameboardParameters: number[][] = [[0,1,2,3,4,5,6,7,8], [0,1,2,3,4,5,6,7,8]];
 
-    for (let i = 0; i < main[0].length; i++) {
-      for (let j = 0; j < main[1].length; j++) {
-        temp.push([j, i]);
+    for (let i = 0; i < tdaGameboardParameters[0].length; i++) {
+      for (let j = 0; j < tdaGameboardParameters[1].length; j++) {
+        tdaGameboard.push([j, i]);
       }
     }
     
-    this.setState({gArr: temp});
+    this.setState({gArr: tdaGameboard});
   };
 
   // Cleanup
@@ -71,24 +90,13 @@ class App extends React.Component<{}, gameArr>{
   createMines = (sSet: Set<number>, iArr: Array<number[]>, fClick: number) => {
 
     let posArr = pArr(iArr, fClick);
-    let posSet = new Set();
+    let posSet: Set<number> = new Set();
+    let rIndex;
 
-    posArr.forEach((item) => {
-      if (item !== undefined) {
-        if (iArr[fClick][0] === 0 && item[0] - 9 === -1) {
-          return;
-        } else if (iArr[fClick][0] === 8 && item[0] - 1 === -1) {
-          return;
-        } else {
-          posSet.add(iArr.indexOf(item));
-        }
-      } else if (item === undefined) {
-        return;
-      }
-    })
+    borderCheck(posArr, iArr, fClick, posSet);
 
     for (let i = 0; i < 10; i++) {
-      let rIndex = Math.floor(Math.random() * iArr.length);
+      rIndex = Math.floor(Math.random() * iArr.length);
       if (rIndex === fClick || sSet.has(rIndex) || posSet.has(rIndex)) {
         i--;
       } else {
@@ -100,32 +108,42 @@ class App extends React.Component<{}, gameArr>{
   initMines = (gameboardArr: Array<number[]>, fClick: number) => {
     let tempStateSet: Set<number> = new Set();
     this.createMines(tempStateSet, gameboardArr, fClick);
+    this.initClues(gameboardArr, tempStateSet, fClick);
     this.setState({mArr: tempStateSet});
-    this.initClues(this.state.gArr, tempStateSet, fClick);
   }
   
   initClues = (input: Array<number[]>, tempInput: any, fClick: number) => {
-    let clues = [];
+    let clues: gameState["cArr"] = [];
+    let tempClues: Array<any>;
+    let tempArr;
+    let x;
+    let index: number;
 
     input.forEach((item) => {
-      let index = input.indexOf(item);
+      index = input.indexOf(item)
+      tempArr = pArr(input, index);
+      tempClues = [];
+      x = 0;
+
+      borderCheck(tempArr, input, index, tempClues);
+
       if (tempInput.has(index)) {
-        console.log(index, "mine")
-        return;
+        clues.push(null);
       } else {
-        let tempArr = pArr(input, index);
-        let x = 0;
-        console.log(tempArr)
-        for (let i = 0; i < tempArr.length; i++) {
-          if (tempInput.has(tempArr[i])) {
-            x += 1;
-            console.log(x);
+        for (let i = 0; i < tempClues.length; i++) {
+          if (tempInput.has(tempClues[i]) === true) {
+            x++;
           }
         }
-
-        x = 0;
+        clues.push(x);
       }
     })
+
+    this.setState({cArr: clues});
+  }
+
+  sweepMines = (clickValue: number) => {
+    console.log(this.state.cArr[clickValue])
   }
 
   clicked = (gArrPos: number[]) => {
@@ -149,7 +167,7 @@ class App extends React.Component<{}, gameArr>{
                 name={"g_btn"}
                 key={index} 
                 onClick={() => this.clicked(item)}>
-                {item}{this.state.mArr.has(index) && 1}
+                {/* {item}{this.state.mArr.has(index) && 1} */}
               </button>
             );
           })}
